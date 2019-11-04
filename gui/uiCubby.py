0412025d84ca90ba74de54c6bd6e20b4851fb9e9
Alexander P Leones <leonesa@sp2.ecs.csus.edu>
2019-11-04 12:52:32 -0800

    added another cubby button

--- gui/uiCubby.py
+++ gui/uiCubby.py
@@ -15,7 +15,7 @@ cubby3=16
 cubby4=12
 
 space1=False #space 1 is initially empty
-
+space2=False
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(cubby1, GPIO.OUT,initial=GPIO.LOW)
 GPIO.setup(cubby2, GPIO.OUT,initial=GPIO.LOW)
@@ -28,20 +28,29 @@ master.geometry("600x100")
 
 ONlabel1 = Label(master, text="Cubby Empty (system start)")
 ONlabel1.grid(row=0, column=0)
-print(space1)
+ONlabel2 = Label(master, text="Cubby Empty (system start)")
+ONlabel2.grid(row=0, column=1)
 
 def cubby1Button():
 	global space1
 	if(space1==False):
 		GPIO.output(cubby1, GPIO.HIGH) #signal storage op.
 		space1=True           #space1 is occupied? True
-		print(space1)
 		ONlabel1.config(text = "Cubby ocupied") #Display it's full
 	else:
 		GPIO.output(cubby1,GPIO.LOW)
 		space1=False
-		print(space1)
-		ONlabel1.config(text="Cubby Empty")
+		ONlabel1.config(text="Cubby Empty")	
+def cubby2Button():
+	global space2
+	if(space2==False):
+		GPIO.output(cubby2, GPIO.HIGH) #signal storage op.
+		space2=True           #space1 is occupied? True
+		ONlabel2.config(text = "Cubby ocupied") #Display it's full
+	else:
+		GPIO.output(cubby2,GPIO.LOW)
+		space2=False
+		ONlabel2.config(text="Cubby Empty")
 		
 def ExitButton():
 	Exitbutton = Button(master, text="Exit", command= EandD)
@@ -52,5 +61,7 @@ def EandD():
 
 Cubby1StoreButton = Button(master, text="GPIO 21 Cubby 1", command= cubby1Button)
 Cubby1StoreButton.grid(row=1, column=0)
+Cubby1StoreButton = Button(master, text="GPIO 20 Cubby 2", command= cubby2Button)
+Cubby1StoreButton.grid(row=1, column=1)
 ExitButton()
 master.mainloop()
