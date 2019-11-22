1263a2537242825cc5e76f73ac4f16f592a14919
Angelica Smith-Evans <angelsmithevans@gmail.com>
2019-11-22 14:37:52 -0800

    Changed pin assignments, added comments

--- chassis/sketch_nov15a/proximity_sensor_example.ino
+++ chassis/sketch_nov15a/proximity_sensor_example.ino
@@ -1,16 +1,19 @@
-//collects data from an analog sensor
+/* 
+The purpose of this code is to test an IR Proximity Sensor component to check if it is working.
+The IR Sensor is connected to pin A0, 5v source, and ground of the arduino UNO board.
+*/
 
-int sensorpin = 0;                 // analog pin used to connect the sharp sensor
-int val = 0;                 // variable to store the values from sensor(initially zero)
+int sensorpin = A0;          // A0 pin connects the IR sensor
+int val = 0;                 // variable that stores the values from the IR sensor (initially zero)
 
 void setup()
 {
-  Serial.begin(9600);               // starts the serial monitor
+  Serial.begin(9600);             // starts the serial monitor (data rate=9600bps)
 }
  
 void loop()
 {
-  val = analogRead(sensorpin);       // reads the value of the sharp sensor
-  Serial.println(val);            // prints the value of the sensor to the serial monitor
-  delay(400);                    // wait for this much time before printing next value
+  val = analogRead(sensorpin);    // reads the value of the IR sensor, stores in variable var
+  Serial.println(val);            // prints the value of var to the serial monitor
+  delay(400);                     // wait 400ms before next IR sensor read
 }
