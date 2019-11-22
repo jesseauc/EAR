ec0c4a2ed60dc14b0b04b36ce6d74120ce51ab51
Angelica Smith-Evans <angelsmithevans@gmail.com>
2019-11-22 15:06:42 -0800

    Update and rename chassis/sketch_nov15a/sketch_nov15a.ino to chassis/test_code/motor_control_test.ino

--- chassis/test_code/motor_control_test.ino
+++ chassis/test_code/motor_control_test.ino
@@ -1,14 +1,15 @@
 // taken from online to serve as a motor, H bridge, and arduino test. Don't actually use this for our final code.
 
 // connect motor controller pins to Arduino digital pins
-// motor one
-int enA = 10;
-int in1 = 9;
-int in2 = 8;
-// motor two
-int enB = 5;
-int in3 = 7;
-int in4 = 6;
+// Left Wheel Motor
+int enA = 10;               //enable A pin connected to UNO pin 10 to enable LW Motor Driver
+int in1 = 9;                //in1 pin connected to UNO pin 9 to control LW Motor
+int in2 = 8;                //in2 pin connected to UNO pin 8 to control LW Motor
+// Right Wheel Motor
+int enB = 5;                //enable B pin connected to UNO pin 5 to enable RW Motor Driver
+int in3 = 7;                //in3 pin connected to UNO pin 7 to control RW Motor
+int in4 = 6;                //in4 pin connected to UNO pin 6 to control RW Motor
+
 void setup()
 {
   // set all the motor control pins to outputs
@@ -19,44 +20,54 @@ void setup()
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
 }
+
 void demoOne()
 {
   // this function will run the motors in both directions at a fixed speed
-  // turn on motor A
+  
+  // turn on Left Wheel Motor
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
-  // set speed to 200 out of possible range 0~255
+  
+  // set speed to 180 out of possible range 0~255
   analogWrite(enA, 180);
-  // turn on motor B
+  
+  // turn on Right Wheel Motor
   digitalWrite(in3, HIGH);
   digitalWrite(in4, LOW);
-  // set speed to 200 out of possible range 0~255
+  
+  // set speed to 180 out of possible range 0~255
   analogWrite(enB, 180);
-  delay(600);
+  delay(600);              //wait 600ms
+ 
   // now change motor directions
   digitalWrite(in1, LOW);
   digitalWrite(in2, HIGH);  
   digitalWrite(in3, LOW);
   digitalWrite(in4, HIGH); 
   delay(600);
+  
   //twist one way
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);  
   digitalWrite(in3, LOW);
   digitalWrite(in4, HIGH); 
   delay(600);
+  
   //twist another
   digitalWrite(in1, LOW);
   digitalWrite(in2, HIGH);  
   digitalWrite(in3, HIGH);
   digitalWrite(in4, LOW); 
   delay(600);
+  
   // now turn off motors
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);  
   digitalWrite(in3, LOW);
   digitalWrite(in4, LOW);
 }
+
 void demoTwo()
 {
   // this function will run the motors across the range of possible speeds
@@ -88,8 +99,10 @@ void demoTwo()
   digitalWrite(in3, LOW);
   digitalWrite(in4, LOW);  
 }
+
 void loop()
 {
   demoOne();
-  delay(1000);
+  //demoTwo();    //will not run demoTwo at this stage
+  delay(1000);    //wait 1 sec
 }
